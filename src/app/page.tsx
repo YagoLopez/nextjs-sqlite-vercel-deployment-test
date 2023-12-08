072bae07d1c12ff8fd5c53a9e97e340ec6d7f718
Yago López <yago.lopez@gmail.com>
2023-12-08 22:13:02 +0100

    fix error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,19 @@ export default async function Home() {
   const products = await prisma.product.findMany()
   console.log('products', products)
 
+  const productId = Date.now()
+  const res = await prisma.product.create({data: {
+      "name": "Product" + productId ,
+      "description": "Description product " + productId,
+      "price": "200",
+      "created_at": "2023-12-07T18:40:57.233Z",
+      "updated_at": "1970-01-01T00:00:00.001Z",
+      "created_by_id": 1,
+      "updated_by_id": 1
+    }})
+  console.log('res', res)
+
+
   const onCreateProduct = async ()=>{
     "use server"
     const productId = Date.now()
@@ -21,8 +34,6 @@ export default async function Home() {
       }})
     console.log('res', res)
   }
-
-  onCreateProduct()
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-12">
